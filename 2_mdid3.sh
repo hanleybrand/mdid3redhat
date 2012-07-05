552fe9a9844f431d21e4fa506278eb50a0c32988
hanleybrand <phanley@temple.edu>
2012-07-05 18:54:17 -0400

    fixed the yum commands (test this)

--- 2_mdid3.sh
+++ 2_mdid3.sh
@@ -18,20 +18,23 @@ mkdir errors
 # 2> send errors to a text file in the errors dir, -y says "yes" to questions
 echo 'yum...'
 sudo yum install openjdk-6-jre-headless -y 2> errors/openjdk-6-jre-headless-errors.txt
-sudo yum install python-setuptools -y 2> errors/python-setuptools-errors.txt
-sudo yum install libjpeg62-dev -y  2> errors/libjpeg62-errors.txt
-sudo yum install unixodbc -y  2> errors/unixodbc-errors.txt
-sudo yum install unixodbc-dev -y  2> errors/unixodbc-dev-errors.txt
-sudo yum install freetds-dev -y  2> errors/freetds-errors.txt
-sudo yum install tdsodbc -y  2> errors/tdsodbc-errors.txt
-sudo yum install python-dev -y  2> errors/python-dev-errors.txt
-sudo yum install libmysqlclient16-dev -y  2> errors/libmysqlclient16-errors.txt
-sudo yum install python-ldap -y  2> errors/python-ldap-errors.txt
-sudo yum install python-memcache -y  2> errors/python-memcache-errors.txt
-sudo yum install memcached -y  2> errors/memcached-errors.txt
-sudo yum install libapache2-mod-wsgi -y  2> errors/libapache2-mod-wsgi-errors.txt
-sudo yum install g++  -y 2> errors/g++-errors.txt
-sudo yum install mysql-server -y  2> errors/mysql-server-errors.txt
+
+	sudo yum install python26 -y 2> errors/python26.txt
+	sudo yum install python26-devel -y  2> errors/python26-devel.txt
+	sudo yum install python26-setuptools -y 2> errors/python26-setuptools.txt
+	sudo yum install java-1.6.0-openjdk  -y 2> errors/java-1.6.0-openjdk.txt
+	sudo yum install libjpeg-devel -y 2> errors/libjpeg-devel.txt
+	sudo yum install unixODBC -y 2> errors/unixODBC.txt
+	sudo yum install unixODBC-devel -y 2> errors/unixODBC-devel.txt
+	sudo yum install freetds-devel -y  2> errors/freetds-devel.txt
+	sudo yum install MySQL-python26 -y 2> errors/MySQL-python26.txt
+	sudo yum install python-ldap -y 2> errors/python-ldap.txt
+	sudo yum install memcached -y 2> errors/memcached
+	sudo yum install python-memcached -y 2> errors/python-memcached.txt
+	sudo yum install mod_wsgi -y 2> errors/mod_wsgi.txt
+	sudo yum install gcc-c++  -y 2> errors/gcc.txt
+	sudo yum install pyodbc -y 2> errors/pyodbc.txt
+
 echo 'easy-installing...'
 sudo easy_install pyodbc 2> errors/pyodbc-errors.txt
 sudo easy_install mysql-python 2> errors/pyodbc-errors.txt
